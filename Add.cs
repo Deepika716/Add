18fb6a768f484dd30b3ad2311e454968f403420d
Deepika716 <deepikarao716@gmail.com>
2024-10-05 14:49:41 +0530

    Add files via upload

---
+++ Add.cs
@@ -0,0 +1,19 @@
+using System;
+struct Sum{
+
+public static void Main(string[] args){
+Sum s=new Sum();
+int a = 8;
+int b = 2;
+int add=a+b;
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
+Console.WriteLine("add: " +add);
+Console.WriteLine("sub: " +sub);
+Console.WriteLine("mul: " +mul);
+Console.WriteLine("div: " +div);
+
+}
+
+}
